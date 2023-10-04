e852a0818299467af23ae54707489ac6fd5663e9
tibocst <thibault.coste.233@gmail.com>
2023-10-04 17:02:09 +0200

    modif packages + connection

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb://127.0.0.1/argentBankDB'
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
